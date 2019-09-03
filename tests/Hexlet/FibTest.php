400aeb1ed98b44061d0f7f1354a5e45dd28f68be
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-03 16:59:07 +0300

    update fibTest

--- tests/Hexlet/FibTest.php
+++ tests/Hexlet/FibTest.php
@@ -4,6 +4,7 @@ namespace Learning\Php\Hexlet\Tests;
 
 use PHPUnit\Framework\TestCase;
 use function Learning\Php\Hexlet\Fib\fib;
+use function Learning\Php\Hexlet\Fib\fib2;
 
 class FibTest extends TestCase
 {
@@ -14,12 +15,10 @@ class FibTest extends TestCase
     public function testFib($value, $num)
     {
         $this->assertEquals($value, fib($num));
+        $this->assertEquals($value, fib2($num), "fib2");
     }
     
-    public function testFib2($value, $num)
-    {
-        $this->assertEquals($value, fib($num));
-    }
+   
     
     public function additionProvider()
     {

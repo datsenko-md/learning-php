231a6112bf956c968dd959fdb559cbe24301294d
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-04 10:29:55 +0300

    add swapElementsInArray

---
+++ tests/Hexlet/SwapElementsInArrayTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Learning\Php\Hexlet\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Learning\Php\Hexlet\SwapElementsInArray\swapElementsInArray;
+
+class SwapElementsInArrayTest extends TestCase
+{
+    public function testSwapElementsInArray()
+    {
+        $names = ["Bob", "Carl", "David"];
+        
+        $this->assertEquals(array_reverse($names), swapElementsInArray($names, 1));
+        $this->assertEquals($names, swapElementsInArray($names, 0));
+        $this->assertEquals($names, swapElementsInArray($names, 2));   
+    }
+}

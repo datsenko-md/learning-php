1a7a61f44a7bc3759e947a660965ce063979f4a9
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-04 11:54:53 +0300

    add isContinuousSequence

---
+++ tests/Hexlet/IsContinuousSequenceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Learning\Php\Hexlet\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Learning\Php\Hexlet\IsContinuousSequence\isContinuousSequence;
+
+class IsContinuousSequenceTest extends TestCase
+{
+    public function testIsContinuousSequence()
+    {
+        $this->assertTrue(isContinuousSequence([5, 6, 7, 8, 9]));
+        $this->assertTrue(isContinuousSequence([0, 1, 2, 3, 4]));
+        $this->assertTrue(isContinuousSequence([-4, -3, -2 , -1]));
+        
+        
+        $this->assertFalse(isContinuousSequence([5, 6, 8, 9]));
+        $this->assertFalse(isContinuousSequence([5, 6, 8, 8, 9]));
+        $this->assertFalse(isContinuousSequence([]));
+    }
+}

f96150231a4699791e9c0542a7fe369808f57acb
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-06 13:10:51 +0300

    add makeCensored

---
+++ tests/Hexlet/Arrays/MakeCensoredTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Php\Hexlet\Basics\Challenges\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Php\Hexlet\Basics\Arrays\MakeCensored\makeCensored;
+
+class MakeCensoredTest extends TestCase
+{
+    public function testMakeCensored()
+    {
+        $sentence = 'When you play the game of thrones, you win or you die';
+        $actual = makeCensored($sentence, ['die', 'play']);
+        $expected = 'When you $#%! the game of thrones, you win or you $#%!';
+        $this->assertEquals($expected, $actual);
+        
+        $sentence2 = 'chicken chicken? chicken! chicken';
+        $actual2 = makeCensored($sentence2, ['?', 'chicken']);
+        $expected2 = '$#%! chicken? chicken! $#%!';
+        $this->assertEquals($expected2, $actual2);
+    }
+}

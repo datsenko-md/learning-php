3f0edad9cadc831b32a5ecdfd8a032bb9a24a49c
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-07 12:40:55 +0300

    add FizzBuzz

---
+++ src/Hexlet/Basics/Challenges/FizzBuzz.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Функиця реализует игру FizzBuzz
+*/
+
+namespace Php\Hexlet\Arrays\FizzBuzz;
+
+function fizzBuzz($begin, $end)
+{
+    $result = [];
+    
+    for ($i = $begin; $i <= $end; $i++) {
+        if (($i % 3 === 0) && ($i % 5 === 0)) {
+            $result[] = 'FizzBuzz';
+        } elseif ($i % 3 === 0) {
+            $result[] = 'Fizz';
+        } elseif ($i % 5 === 0) {
+            $result[] = 'Buzz';
+        } else {
+            $result[] = $i;
+        }
+    }
+    
+    return implode(' ', $result);
+}

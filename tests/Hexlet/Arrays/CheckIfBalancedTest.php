c9bfc8537bbc82359821cf9050778062d20d8213
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-07 10:26:03 +0300

    add checkIfBalanced

---
+++ tests/Hexlet/Arrays/CheckIfBalancedTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Php\Hexlet\Arrays\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Php\Hexlet\Arrays\CheckIfBalanced\checkIfBalanced;
+
+class CheckIfBalancedTest extends TestCase
+{
+    public function testCheckIfBalanced()
+    {
+        $data1 = '(3 + 2) / (2 + 7)*(2 + 5)';
+        $this->assertTrue(checkIfBalanced($data1));
+        
+        $data2 = '(3 + 2))';
+        $this->assertFalse(checkIfBalanced($data2));
+    }
+}

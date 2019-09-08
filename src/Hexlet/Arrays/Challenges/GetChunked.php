19be8e4e51fcea0ac2849c72043c80812e99f7cc
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-08 12:24:48 +0300

    add getChunked

---
+++ src/Hexlet/Arrays/Challenges/GetChunked.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+Функция выполняет чанкование входного массива
+*/
+
+namespace Php\Hexlet\Arrays\Challenges\GetChunked;
+
+/*
+function getChunked(array $arr, $sizeOfChunk)
+{
+    $count = 0;
+    $result = [];
+    $chunk = [];
+    foreach ($arr as $value) {
+        if ($count === $sizeOfChunk) {
+            $result[] = $chunk;
+            $chunk = [];
+            $count = 0;
+        }
+
+        $chunk[] = $value;
+        $count++;
+    }
+
+    if (!empty($chunk)) {
+        $result[] = $chunk;
+    }
+
+    return $result;
+}
+*/
+
+function getChunked(array $arr, $size)
+{
+    $result = [];
+    
+    for ($i = 0, $length = count($arr); $i < $length; $i += $size) {
+        $result[] = array_slice($arr, $i, $size);
+    }
+    
+    return $result;
+}
---
+++ tests/Hexlet/Arrays/Challenges/GetChunkedTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Php\Hexlet\Arrays\Challenges\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Php\Hexlet\Arrays\Challenges\GetChunked\getChunked;
+
+class GetChunkedTest extends TestCase
+{
+    public function testGetChunked()
+    {
+        $actual1 = getChunked(['a', 'b', 'c', 'd'], 2);
+        $expected1 = [['a', 'b'], ['c', 'd']];
+        $this->assertEquals($expected1, $actual1);
+        
+        $actual2 = getChunked(['a', 'b', 'c', 'd'], 3);
+        $expected2 = [['a', 'b', 'c'], ['d']];
+        $this->assertEquals($expected2, $actual2);
+    }
+}

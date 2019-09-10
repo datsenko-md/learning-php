e4d964d7a6b107352265337cfea5299d0e021210
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-10 06:47:43 +0300

    add from pairs

---
+++ tests/Hexlet/AssociativeArrays/Challenges/FromPairsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Php\Hexlet\AssociativeArrays\Challenges\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Php\Hexlet\AssociativeArrays\Challenges\FromPairs\fromPairs;
+
+class FromPairsTest extends TestCase
+{
+    public function testFromPairs()
+    {
+        $actual = fromPairs([['cat', 5], ['dog', 6], ['cat', 11]]);
+        $expected = ['dog' => 6, 'cat' => 11];
+        $this->assertEquals($expected, $actual);
+    }
+}

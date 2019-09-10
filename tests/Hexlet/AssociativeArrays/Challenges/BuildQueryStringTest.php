25a75a625732db59f0a27fc19ce2cd479621fb94
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-10 07:12:12 +0300

    add buildQueryString

---
+++ tests/Hexlet/AssociativeArrays/Challenges/BuildQueryStringTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Php\Hexlet\AssociativeArrays\Challenges\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Php\Hexlet\AssociativeArrays\Challenges\BuildQueryString\buildQueryString;
+
+class BuildQueryStringTest extends TestCase
+{
+    public function testBuildQueryString()
+    {
+        $actual = buildQueryString(['per' => 10, 'page' => 1 ]);
+        $expected = 'page=1&per=10';
+        $this->assertEquals($expected, $actual);
+    }
+}

6267308c26fe2e536008b1d4598879108a4e7cc8
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-14 15:59:54 +0300

    add slugify

---
+++ tests/Hexlet/Functions/SlugifyTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Php\Hexlet\Functions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Funct\Strings;
+use function Php\Hexlet\Functions\Slugify\slugify;
+
+class SlugifyTest extends TestCase
+{
+    public function testSlugify()
+    {
+        $data = 'Hello World';
+        $actual = slugify($data);
+        $expected = Strings\slugify($data);
+        $this->assertEquals($expected, $actual);
+    }
+}

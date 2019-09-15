00d7adaad46f0f4e2d2ff53352a385d3a8182602
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-15 09:51:00 +0300

    add takeOldest

---
+++ tests/Hexlet/Functions/TakeOldestTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Php\Hexlet\Functions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Php\Hexlet\Functions\TakeOldest\takeOldest;
+
+class TakeOldestTest extends TestCase
+{
+    public function testTakeOldest()
+    {
+        $users = [
+            ['name' => 'Tirion', 'birthday' => '1988-11-19'],
+            ['name' => 'Sam', 'birthday' => '1999-11-22'],
+            ['name' => 'Rob', 'birthday' => '1975-01-11'],
+            ['name' => 'Sansa', 'birthday' => '2001-03-20'],
+            ['name' => 'Tisha', 'birthday' => '1992-02-27']
+        ];
+        
+        $actual1 = takeOldest($users);
+        $expected1 = [['name' => 'Rob', 'birthday' => '1975-01-11']];
+        $this->assertEquals($expected1, $actual1);
+        
+        $actual2 = takeOldest($users, 2);
+        $expected2 = [['name' => 'Rob', 'birthday' => '1975-01-11'],
+                     ['name' => 'Tirion', 'birthday' => '1988-11-19']];
+        $this->assertEquals($expected2, $actual2);
+        
+    }
+}

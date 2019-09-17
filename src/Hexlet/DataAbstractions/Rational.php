f45af81df9343897c4dd03663fb08e91a121ca7b
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-17 22:09:59 +0300

    add rational

---
+++ src/Hexlet/DataAbstractions/Rational.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Php\Hexlet\DataAbstractions\Rational;
+
+use function Php\Hexlet\DataAbstractions\Gcd\gcd;
+
+function makeRational($numer, $denom)
+{
+    $gcd = gcd($numer, $denom);
+    return [
+        'numer' => $numer / $gcd,
+        'denom' => $denom / $gcd
+    ];
+}
+
+function getNumer($rational)
+{
+    return $rational['numer'];
+}
+
+function getDenom($rational)
+{
+    return $rational['denom'];
+}
+
+function ratToString($rat)
+{
+    return getNumer($rat) . '/' . getDenom($rat);
+}
+
+function add($rat1, $rat2)
+{
+    $numer = getNumer($rat1) * getDenom($rat2) + getNumer($rat2) * getDenom($rat1);
+    $denom = getDenom($rat1) * getDenom($rat2);
+    return makeRational($numer, $denom);
+}
+
+function sub($rat1, $rat2)
+{
+    $numer = getNumer($rat1) * getDenom($rat2) - getNumer($rat2) * getDenom($rat1);
+    $denom = getDenom($rat1) * getDenom($rat2);
+    return makeRational($numer, $denom);
+}
---
+++ tests/Hexlet/DataAbstractions/RationalTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Php\Hexlet\DataAbstractions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Php\Hexlet\DataAbstractions\Rational\makeRational;
+use function Php\Hexlet\DataAbstractions\Rational\getNumer;
+use function Php\Hexlet\DataAbstractions\Rational\getDenom;
+use function Php\Hexlet\DataAbstractions\Rational\ratToString;
+use function Php\Hexlet\DataAbstractions\Rational\add;
+use function Php\Hexlet\DataAbstractions\Rational\sub;
+
+class RationalTest extends TestCase
+{
+    public function testRational()
+    {
+        $a = makeRational(10, 20);
+        $this->assertEquals(1, getNumer($a));
+        $this->assertEquals(2, getDenom($a));
+    }
+    
+    public function testRatToString()
+    {
+        $a = makeRational(10, 20);
+        $this->assertEquals('1/2', ratToString($a));
+        
+    }
+    
+    public function testAddAndSub()
+    {
+        $a = makeRational(10, 20);
+        $b = makeRational(1, 3);
+        
+        $sum = makeRational(5, 6);
+        $diff = makeRational(1, 6);
+        
+        $this->assertEquals($sum, add($a, $b));
+        $this->assertEquals($diff, sub($a, $b));
+    }
+}

7e331959ba909b5f45abf6890a32ce49a94453a4
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-17 17:54:32 +0300

    add calculateDistance

---
+++ tests/Hexlet/DataAbstractions/CalculateDistanceTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Php\Hexlet\DataAbstractions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Php\Hexlet\DataAbstractions\CalculateDistance\calculateDistance;
+
+class CalculateDistanceTest extends TestCase
+{
+    public function testCalculateDistance()
+    {
+        $point1 = [0, 0];
+        $point2 = [3, 4];
+        $this->assertEquals(5, calculateDistance($point1, $point2));
+    }
+}

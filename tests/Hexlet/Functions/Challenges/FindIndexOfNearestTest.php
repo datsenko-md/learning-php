29514f65f0f6829eb8153de48ea43a73e9d0f057
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-17 13:25:59 +0300

    add findIndexOfNearest

---
+++ tests/Hexlet/Functions/Challenges/FindIndexOfNearestTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Php\Hexlet\Functions\Challenges\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Php\Hexlet\Functions\Challenges\FindIndexOfNearest\findIndexOfNearest;
+
+class FindIndexOfNearestTest extends TestCase
+{
+    public function testFindIndexOfNearest()
+    {
+        $this->assertNull(findIndexOfNearest([], 2));
+        $this->assertEquals(2, findIndexOfNearest([15, 10, 3, 4, 3], 0));
+    }
+}

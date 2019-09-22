00c2abe749bc84f1986ba96ec846ef622edc2245
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-22 10:49:21 +0300

    add url

---
+++ src/Hexlet/OOPBasics/Challenges/Url.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Php\Hexlet\OOPBasics\Challenges;
+
+class Url implements UrlInterface
+{
+    private $data;
+    
+    public function __construct($url)
+    {
+        $this->data = parse_url($url);
+        $queryParams = [];
+        if (isset($this->data['query'])) {
+            parse_str($this->data['query'], $queryParams);
+        }
+        $this->data['query'] = $queryParams;
+    }
+    
+    public function getScheme()
+    {
+        return $this->data['scheme'];
+    }
+    
+    public function getHost()
+    {
+        return $this->data['host'];
+    }
+    
+    public function getQueryParams()
+    {
+        return $this->data['query'];
+    }
+    
+    public function getQueryParam($key, $defaultValue = null)
+    {
+        return $this->data['query'][$key] ?? $defaultValue;
+    }
+}

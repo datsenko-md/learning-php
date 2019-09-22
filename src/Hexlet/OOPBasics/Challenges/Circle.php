30e853a3a190470aeb0ceb08cb5662d6958238b4
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-22 09:28:56 +0300

    add circle

---
+++ src/Hexlet/OOPBasics/Challenges/Circle.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Php\Hexlet\OOPBasics\Challenges;
+
+class Circle
+{
+    private $radius;
+    
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+    
+    public function getRadius()
+    {
+        return $this->radius;
+    }
+    
+    public function getArea()
+    {
+        return pi() * ($this->radius ** 2);
+    }
+    
+    public function getCircumference()
+    {
+        return 2 * pi() * $this->radius;
+    }
+}

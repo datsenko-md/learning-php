51746d3074e79eb2fc78b402f960e44763748263
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-22 13:33:35 +0300

    add passwordValidator

---
+++ src/Hexlet/OODesign/PasswordValidator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Php\Hexlet\OODesign;
+
+class PasswordValidator
+{
+    const OPTIONS = [
+        'minLength' => 8,
+        'containNumbers' => false
+    ];
+    
+    private $options;
+    
+    public function __construct($options = [])
+    {
+        $this->options = array_merge(self::OPTIONS, $options);
+    }
+    
+    private function hasNumber($subject)
+    {
+        return strpbrk($subject, '1234567890') !== false;
+    }
+    
+    public function validate(string $pass)
+    {
+        $errors = [];
+        
+        if (strlen($pass) < $this->options['minLength']) {
+            $errors['minLength'] = 'too small';
+        }
+        
+        if ($this->options['containNumbers']) {
+            if (! $this->hasNumber($pass)) {
+                $errors['containNumbers'] = 'should contain at least one number';
+            }
+        }
+        
+        return $errors;
+    }
+}

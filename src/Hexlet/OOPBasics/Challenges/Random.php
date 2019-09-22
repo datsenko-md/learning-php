34aaf4378c571f839ddda0573a5d2ffbadba8e62
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-22 11:19:59 +0300

    add random

---
+++ src/Hexlet/OOPBasics/Challenges/Random.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Php\Hexlet\OOPBasics\Challenges;
+
+class Random
+{
+    private $a = 106;
+    private $c = 1283;
+    private $m = 6075;
+    private $x;
+    private $beginValue;
+    
+    public function __construct($beginValue)
+    {
+        $this->beginValue = $beginValue;
+        $this->x = $beginValue;
+    }
+    
+    public function getNext()
+    {
+        $a = $this->a;
+        $c = $this->c;
+        $m = $this->m;
+        $x = $this->x;
+        $next = (($a * $x) + $c) % $m;
+        
+        $this->x = $next;
+        return $next;
+    }
+    
+    public function reset()
+    {
+        $this->x = $this->beginValue;
+    }
+}

8c0c3a21d02c1fa044912cf714b42995fb2db450
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-23 13:23:40 +0300

    add deckOfCards

---
+++ src/Hexlet/OODesign/DeckOfCards.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Php\Hexlet\OODesign;
+
+use Tightenco\Collect\Support;
+
+class DeckOfCards
+{
+    private $deck;
+    
+    public function __construct(array $cards)
+    {
+        $this->deck = collect($cards);
+    }
+    
+    public function getFullDeck()
+    {
+        return $this->deck->map(function ($card) {
+//            return [$card, $card, $card, $card];
+            return array_fill(0, 4, $card);
+        })->collapse();
+    }
+    
+    public function getShuffled()
+    {
+        $fullDeck = collect($this->getFullDeck());
+        return $fullDeck->shuffle()->all();
+    }
+}
